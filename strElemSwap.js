3669a6dd92c5cd0a4e47c6b8509cfbf7b1857980
Shuvadeep Podder <shuvadeep.podder@corp.csmpl.com>
2025-03-11 21:48:40 +0530

    code pracice

--- strElemSwap.js
+++ strElemSwap.js
@@ -6,4 +6,5 @@ let str = "biswanath shuvashree shuvadeep rinku";
 let cnvrtArr = str.split(" ");
 let remove = cnvrtArr.splice(2, 1);
 let positionChange = cnvrtArr.splice(1,0, remove[0]);
-console.log(cnvrtArr);
+let result = cnvrtArr.join(" ");
+console.log(result);
